73bcdc456b896fc31f3e3bc5b5e34ba66b7c47eb
Royalboe <auomidejohnson@gmail.com>
2021-11-27 00:46:23 +0100

    initial commit

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,20 +1,31 @@
 package calculator
 
+val stack = Stack()
+val postFixExpressions = mutableListOf<String>()
+val digitsMap = mutableMapOf<String, Int>()
+
 fun main() {
-    val digitsMap = mutableMapOf<String, Int>()
     while (true) {
         val input = readLine()!!
         when {
-            input == "" -> continue
-            input.contains("/") -> {
+            input.isBlank() -> continue
+            input[0] == '/' -> {
                 if (commands(input)) {
                     digitsMap.clear()
                     break
                 }
             }
-            input.isNotEmpty() -> {
+            input.contains("(") && !input.contains(")")
+                    || input.contains(")") && !input.contains("(")
+                    || input.contains("[*]{2,}".toRegex())
+                    || input.contains("[/]{2,}".toRegex()) -> {
+                println("Invalid expression")
+            }
+            else -> {
                 try {
-                    compute(input, digitsMap)
+                    val formattedString = input.reformatString()
+                    decompile(formattedString)
+                    compute(postFixExpressions)
                 } catch (e: Exception) {
                     println("Invalid expression")
                 }
@@ -30,7 +41,7 @@ private fun commands(line: String): Boolean {
             return true
         }
         "/help" -> {
-            println("This program is a calculator with addition and subtraction functionalities")
+            println("This program is a basic calculator that allows for storing of values as variables")
         }
         else -> {
             println("Unknown command")
@@ -39,107 +50,148 @@ private fun commands(line: String): Boolean {
     return false
 }
 
-private fun compute(line: String, identifiers: MutableMap<String, Int>) {
-    val regex = Regex("""\s*""")
-    val newLine = line.replace(regex, "")
-    val addWords = mutableListOf<String>()
-    val invalidRegex = Regex("([\\d]+[+|-]+)")
-    when {
-        line.contains("=") -> {
-            // To assign variables to store values
-            assignVariables(newLine, identifiers)
+fun String.reformatString(): List<String> {
+    var input = this
+    input = input.replace("[+]+".toRegex(), "+")
+    input = input.replace("---", "-")
+    input = input.replace("--", "+")
+    input = input.replace("----", "+")
+    input = input.replace("------", "+")
+    input = input.replace("-----", "-")
+    for (w in listOf("+", "=", "*", "/", "(", ")")) {
+        input = input.replace(w, " $w ")
+    }
+    input = input.replace("\\s+".toRegex(), " ").trim()
+    return input.split(" ")
+}
+
+fun String.operandToInt(): Int = if (this.isDigit()) this.toInt() else digitsMap[this] ?: 0
+
+fun String.isDigit() = this.matches("[-+]?[0-9]+\\b".toRegex())
+
+fun String.isKey() = this.matches("[a-zA-Z]+\\b".toRegex())
+
+fun assign(k: String, v: String) {
+    val isKeyValid = k.isKey()
+    val isValueValid = v.isKey()
+    val isValueNumber = v.isDigit()
+
+    if (!isKeyValid) {
+        println("Invalid identifier")
+    }
+    if (!isValueNumber) { // not number
+        if (!isValueValid) {
+            println("Invalid assignment")
         }
-        line.matches(invalidRegex) -> {
-            println("Invalid expression")
+        if (!digitsMap.containsKey(v)) {
+            println("Unknown variable")
         }
-        !line.contains(" ") -> {
-            if (line.matches("[+][\\d]+".toRegex())) {
-                println(line.replace("+", ""))
-            } else if (line.matches("[a-zA-Z]+".toRegex())) {
-                if (identifiers.containsKey(line)) {
-                    println(identifiers[line])
-                } else {
-                    println("Unknown variable")
-                }
-            }else println(line)
-        }
-        line.contains("[-|+]".toRegex()) && line.contains("[a-zA-Z]+".toRegex()) -> {
-            var lines = ""
-            for (element in line.split(" ")) {
-                lines += if (element.matches("[a-zA-Z]+".toRegex())) {
-                    "${identifiers[element]!!} "
-                } else {
-                    "$element "
+    }
+    digitsMap[k] = if (isValueNumber) v.toInt() else digitsMap[v] ?: 0
+
+}
+
+fun compute(list: List<String>) {
+    var result = 0
+    var isCalc = false
+    var isAss = false
+    val operands = mutableListOf<String>()
+    for (w in list) {
+        if (w in listOf("+", "-", "*", "/", "=")) {
+            isCalc = true
+            val operand1 = operands[operands.lastIndex - 1]
+            val operand2 = operands[operands.lastIndex]
+            when (w) {
+                "+" -> result = operand1.operandToInt() + operand2.operandToInt()
+                "-" -> result = operand1.operandToInt() - operand2.operandToInt()
+                "*" -> result = operand1.operandToInt() * operand2.operandToInt()
+                "/" -> result = operand1.operandToInt() / operand2.operandToInt()
+                "=" -> {
+                    isAss = true
+                    assign(operand1, operand2)
                 }
             }
-            addWords.addAll(getDigits(lines))
-            addWords.removeLast()
-            println(performComputation(addWords))
+            operands.removeAt(operands.lastIndex)
+            operands[operands.lastIndex] = result.toString()
+        } else {
+            operands.add(w)
         }
-        else -> {
-            addWords.addAll(getDigits(line))
-            val addNumbers = performComputation(addWords)
-            println(addNumbers)
+    }
+    if (!isCalc) {
+        if (digitsMap.containsKey(operands.last())) {
+            result = digitsMap[operands.last()] ?: 0
+        } else {
+            println("Unknown variable")
+            return
         }
+    }
+    if (!isAss) println(result)
+}
+
+fun decompile(list: List<String>) {
+    postFixExpressions.clear()
+    for (w in list) {
+        when (w) {
+            "(" -> stack.push(w)
+            ")" -> while (!stack.isEmpty()) {
+                if (stack.peek() != "(") {
+                    val st = stack.pop().toString()
+                    postFixExpressions.add(st)
+                } else {
+                    stack.pop()
+                    break
+                }
+            }
+            "+" -> pushLow(w)
+            "-" -> pushLow(w)
+            "*" -> pushTop(w)
+            "/" -> pushTop(w)
+            "=" -> pushTop(w)
+            else -> postFixExpressions.add(w)
+        }
+    }
+    while (!stack.isEmpty()) {
+        postFixExpressions.add(stack.pop().toString())
     }
 }
 
-private fun assignVariables(
-    newLine: String,
-    identifiers: MutableMap<String, Int>
-) {
-        val lines = newLine.split("=")
-        when {
-            lines[0].contains("\\d".toRegex()) -> {
-                println("Invalid identifier")
-            }
-            lines[1].contains("[\\d]+".toRegex()) && lines[1].contains("[a-zA-Z]+".toRegex())
-                    || lines.size > 2  -> {
-                println("Invalid assignment")
-            }
-            lines[1].matches("[a-zA-Z]+".toRegex()) -> {
-                if (identifiers.isNotEmpty() && identifiers.containsKey(lines[1])) {
-                    identifiers += lines[0] to identifiers[lines[1]]!!
-                } else {
-                    println("Unknown variable")
-                }
-            }
-            lines[0].matches("[a-zA-Z]+".toRegex()) && lines[1].matches("[\\d]".toRegex()) -> {
-//                identifiers += lines[0] to lines[1].toInt()
-                identifiers[lines[0]] = lines[1].toInt()
-            }
-            else -> println("Invalid identifier")
-        }
-}
-
-private fun performComputation(
-    addWords: MutableList<String>,
-): Int {
-    var addNumbers1 = 0
-        for ((count, i) in addWords.withIndex()) {
-            when (i) {
-                "-" -> {
-                    addNumbers1 -= 2 * (addWords[count + 1].toInt())
-                }
-                else -> addNumbers1 += i.toInt()
-            }
-        }
-       return addNumbers1
-}
-
-private fun getDigits(lined: String): MutableList<String> {
-    val addWords = mutableListOf<String>()
-    for (i in lined.split(" ")) {
-        when {
-            i == " " -> continue
-            i.contains("+") || i.contains("--") && i.length % 2 == 0 -> continue
-            i.contains("--") && i.length % 2 != 0 || i == "-" -> {
-                addWords.add("-")
-            }
-            else -> {
-                addWords.add(i)
-            }
-        }
+fun pushTop(w: String) {
+    while (stack.isNotEmpty() && stack.peek() == "[/|*]".toRegex()) {
+        val st = stack.pop().toString()
+        postFixExpressions.add(st)
     }
-    return addWords
+    stack.size
+    stack.push(w)
+}
+
+fun pushLow(w: String) {
+    while (!stack.isEmpty() && stack.peek() != "(") {
+        val st = stack.pop().toString()
+        postFixExpressions.add(st)
+    }
+    stack.push(w)
+}
+
+class Stack {
+    private val elements: MutableList<Any> = mutableListOf()
+
+    fun isEmpty() = elements.isEmpty()
+
+    fun isNotEmpty() = elements.isNotEmpty()
+
+    val size = elements.size
+
+    fun push(item: Any) = elements.add(item)
+
+    fun pop(): Any? {
+        val item = elements.lastOrNull()
+        if (!isEmpty()) {
+            elements.removeAt(elements.size - 1)
+        }
+        return item
+    }
+
+    fun peek(): Any = elements.last()
+
+    override fun toString(): String = elements.toString()
 }
